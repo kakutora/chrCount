a1bf0a6afc8e6fca6b6f169557378c839ada64ae
kakutora <87274851+kakutora@users.noreply.github.com>
2023-05-22 14:06:38 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import text from "./text.json" assert { type: "json" };
 const t = document.getElementById("inputArea__box");
 const inputInfo = document.querySelectorAll(".input__info");
 const inputBtnCopy = document.querySelector(".inputArea__btn--copy");
@@ -9,10 +10,60 @@ const inputBtnLoad = document.querySelectorAll(".inputArea__btn--load");
 const newsWrapper = document.querySelector(".news");
 const newsNone = document.querySelector(".news__text--none");
 
+if (Object.keys(text.newsList).length) {
+  //jsonからニュース取得
+  newsNone.remove();
+  for (let i = 0; i < Object.values(text.newsList).length; i++) {
+    const createNews = document.createElement("p");
+    createNews.classList.add("news__text");
+    createNews.innerHTML =
+      Object.keys(text.newsList)[i] + " : " + Object.values(text.newsList)[i];
+    newsWrapper.appendChild(createNews);
+  }
+}
+
 t.addEventListener("input", () => {
   chrReset();
   localStorage.setItem("chr", t.value);
 });
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("chr")) {
+    t.innerHTML = localStorage.getItem("chr");
+    chrReset();
+  }
+});
+
+inputBtnCopy.addEventListener("click", () => {
+  //コピー
+  navigator.clipboard.writeText(t.value);
+});
+inputBtnClear.addEventListener("click", () => {
+  //消去
+  t.value = "";
+  chrReset();
+});
+inputBtnClearS.addEventListener("click", () => {
+  //セッションストレージ消去
+  window.sessionStorage.clear();
+});
+inputBtnDownload.addEventListener("click", () => {
+  //txt形式でDL
+  download_txt("chrCount.txt", t.value);
+});
+for (let i = 0; i < inputBtnSave.length; i++) {
+  //セッションストレージへ保存
+  inputBtnSave[i].addEventListener("click", () => {
+    window.sessionStorage.setItem("chrItem" + (i + 1), t.value);
+  });
+}
+for (let i = 0; i < inputBtnLoad.length; i++) {
+  //セッションストレージから読み込み
+  inputBtnLoad[i].addEventListener("click", () => {
+    t.value = window.sessionStorage.getItem("chrItem" + (i + 1));
+    chrReset();
+  });
+}
 
 const chrReset = () => {
   //入力済み文字から情報取得、置き換え
@@ -31,4 +82,17 @@ const chrReset = () => {
       break;
     }
   }
+};
+
+const download_txt = (file_name, data) => {
+  //txt形式でDL
+  const blob = new Blob([data], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.download = file_name;
+  a.href = url;
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
 };
